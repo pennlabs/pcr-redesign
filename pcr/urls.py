b4148021a69e6361efb27a4a4df7d172e21017ea
Eric Wang <ezwang1@gmail.com>
2018-10-14 12:11:16 -0400

    fix leading slash

--- pcr/urls.py
+++ pcr/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('apps.courses.urls')),
+    path('', include('apps.courses.urls')),
     path('api', include('apps.api.urls')),
 ]
 
