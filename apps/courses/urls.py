196681c294df88aab18b57cfe8026f1a33979d62
Jelani Hutchins-Belgrave <jelani@seas.upenn.edu>
2018-10-14 11:34:43 -0400

    Added new URL paths for Courses

---
+++ apps/courses/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path(r'/', TemplateView.as_view(template_name="index.html"), name="index"),
+
+    #pcr_detail urls
+    path('instructor/<ID-NAME>', views.instructor, name="instructor"),
+    path('course/<DEPT-CODE>', views.course, name="course"),
+    path('department/<DEPT>', views.department, name="department"),
+
+    #searchbar URLs
+    path('autocomplete_data.json/<data>', views.autocomplete_data),
+
+    #static URLs
+    path('/logout', views.logout, name="logout"),
+    path('/about', views.about, name="about"),
+    path('/cart', views.cart, name="cart"),
+    path('/faq', views.faq, name="faq"),
+
+    #api TO BE MOVED TO ITS OWN Directory
+    path('/api', views.api, name="api"),
+
+]
--- pcr/urls.py
+++ pcr/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('apps.courses.urls'))
+    path('/', include('apps.courses.urls')),
 ]
