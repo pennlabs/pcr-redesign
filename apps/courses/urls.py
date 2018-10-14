60f4cfe1705bb469ccc453f04d5488dc8a32a1a9
Eric Wang <ezwang1@gmail.com>
2018-10-14 11:45:40 -0400

    fix template import

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path(r'/', TemplateView.as_view(template_name="index.html"), name="index"),
