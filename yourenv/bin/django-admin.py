e5d06202d852383f3a57cbcec3a036cf414df9a2
Eric Wang <ezwang1@gmail.com>
2018-10-21 11:03:35 -0400

    Revert "remove virtual environments"
    This reverts commit 66b7413170e3ed14cf7df5fbeea0b604b4cda6da.

---
+++ yourenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jelani-hutchins-belgrave/Documents/GitHub/pcr/yourenv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
