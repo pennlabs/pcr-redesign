d98e9b4410c43174b901a18be0f7a29212cc1c88
Eric Wang <ezwang1@gmail.com>
2018-10-29 15:44:52 -0400

    remove old file and add models to admin

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Department, CourseHistory, Course, Instructor, Alias, Section, Review, ReviewBit
+
+
+admin.site.register(Department)
+admin.site.register(CourseHistory)
+admin.site.register(Course)
+admin.site.register(Instructor)
+admin.site.register(Alias)
+admin.site.register(Section)
+admin.site.register(Review)
+admin.site.register(ReviewBit)
