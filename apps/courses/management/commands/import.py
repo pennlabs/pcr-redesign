1408c4ab28ccfe9a474ba7133dd54698ebaeb6ae
Eric Wang <ezwang1@gmail.com>
2018-11-04 14:15:42 -0500

    regex fixes and simplifications

--- apps/courses/management/commands/import.py
+++ apps/courses/management/commands/import.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
     help = 'Import PCR data provided by ISC.'
 
     def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
+        super(Command, self).__init__(*args, **kwargs)
         self.courses = []
         self.crosslistings = []
         self.ratings = []
@@ -23,16 +23,17 @@ class Command(BaseCommand):
         src = "".join(options["path"])
         if not os.path.isdir(src):
             raise CommandError("The directory '{}' does not exist!".format(src))
-        self.courses = self.parse(src, "test.sql")
+        self.courses = self.parse(src, "TEST_PCR_COURSE_DESC_V.sql")
         self.crosslistings = self.parse(src, "TEST_PCR_CROSSLIST_SUMMARY_V.sql")
         self.ratings = self.parse(src, "TEST_PCR_RATING_V.sql")
-        self.summaries = self.parse(src, "summary.sql")
+        self.summaries = self.parse(src, "TEST_PCR_SUMMARY_V.sql")
         self.create_departments()
         self.create_instructors()
-        #self.stdout.write(self.style.SUCCESS('Successfully imported data!'))
+        self.stdout.write(self.style.SUCCESS('Successfully imported data!'))
 
     def parse(self, src, sql_in):
-        filename = "{}/" + sql_in
+        filename = os.path.join(src, sql_in)
+        self.stdout.write("Reading from '{}'...".format(filename))
         dicts = []
         with open(filename.format(src), errors='ignore') as f:
             statements = sqlparse.parse(f)
@@ -46,25 +47,20 @@ class Command(BaseCommand):
                             values = str(token)[1:-1]
                             values = re.sub(r'\([^)]*\)', '\'unused_info\'', values)
                             dicts.append(self.create_dictionaries(col_names, values))
+        self.stdout.write("Finished reading from '{}'!".format(filename))
         return dicts
 
     def create_dictionaries(self, col_names, values):
-        entry_dict = {}
-
         left = col_names.index('(')
-        right = col_names.index(')')
+        right = col_names.rindex(')')
         col_names = col_names[left + 1:right]
         col_names = " ".join(col_names.split())
-        col_list = col_names.split(', ')
         col_list = [name.replace("'", "") for name in col_names.split(', ')]
 
-        value_list = re.findall(r"(?:'(.*?)'|([\d.-]+))(?:,|$)", values)
-        value_list = [next(x for x in item if x) for item in value_list]
-        value_list = [value.strip() for value in value_list]
+        value_list = re.findall(r"(?:'(.*?[^'])'|([\d.-]+))(?:,|$)", values)
+        value_list = [next(x for x in item if x).strip() for item in value_list]
 
-        for i in range(len(value_list)):
-           entry_dict[col_list[i]] = value_list[i]
-        return entry_dict
+        return {key: val for key, val in zip(col_list, value_list)}
 
     def create_departments(self):
         for dictionary in self.summaries:
@@ -72,14 +68,14 @@ class Command(BaseCommand):
             name = dictionary["SUBJECT_AREA_DESC"]
             dept, _ = Department.objects.get_or_create(code=code,
                 defaults={"name": name})
-    
+
     def create_instructors(self):
         for dictionary in self.ratings:
             name = dictionary["INSTRUCTOR_NAME"]
             last_name, first_name = name.split(',')
             instructor, _ = Instructor.objects.get_or_create(first_name=first_name, last_name=last_name)
         print(Instructor.objects.all())
-    
+
     def populate_models(self):
         for course, entries in self.courses:
             entries.sort()
